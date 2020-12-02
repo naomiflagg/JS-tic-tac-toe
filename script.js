89833b40423407452b00c0a98f48f547552a8c96
Naomi Flagg <neffly511@gmail.com>
2020-12-02 11:20:26 -0800

    Add game over check

--- script.js
+++ script.js
@@ -1,53 +1,98 @@
 const displayController = (() => {
+  const squares = document.querySelectorAll('.square');
+  const upNext = document.querySelector('.up-next');
+  const gameOverMsg = document.querySelector('.game-over-msg');
   const newGameBtn = document.querySelector('.new-game-btn');
   newGameBtn.addEventListener('click', () => {
-    const game = gameController();
-    game.newGame(); 
+    gameController();
   });
 
-  const squares = document.querySelectorAll('.square');
-
-  function setPlayerMsg(player) {
+  function setUpNext(player) {
     const currentPlayer = document.querySelector('.current-player');
     currentPlayer.textContent = player;
   }
 
-  function displayPlayerMsg() {
-    const playerMsg = document.querySelector('.player-msg');
-    playerMsg.style.display = 'block';
+  function displayUpNext() {
+    upNext.style.display = 'block';
   }
 
   function addToken(square, token) {
     square.textContent = token;
   }
 
-  return { squares, setPlayerMsg, displayPlayerMsg, addToken };
+  function removeUpNext() {
+    upNext.style.display = 'none';     
+  }
+
+  function gameOverMessage(winner = null) {
+    gameOverMsg.textContent = (winner) ? `${winner.name} is the winner!` : "It's a draw!";
+    gameOverMsg.style.display = 'block';
+    removeUpNext();
+  }
+
+  function deactivateSquares() {
+    // squares.forEach(square => {
+    //   let clone = square.cloneNode(true);
+    //   square.parentNode.replaceChild(clone, square);
+    // })
+  }
+
+  function reset() {
+    squares.forEach(square => {
+      square.textContent = '';
+    })
+    gameOverMsg.style.display = 'none';
+  }
+
+  return { 
+    squares, 
+    setUpNext, 
+    displayUpNext, 
+    addToken, 
+    removeUpNext,
+    gameOverMessage,
+    deactivateSquares,
+    reset
+  };
 })();
 
 // Game board module
 const board = (() => {   
+  let boardArray = Array(9);
   function populateArray() {
-    let boardArray = [];
     displayController.squares.forEach(square => {
-      boardArray.push(square.textContent);
+      let index = square.classList[0];
+      boardArray[index] = square.textContent;
     })
     return boardArray;
   }
 
-  return { populateArray };
-})();
+  const possibleWins = [
+    [0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7],
+    [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]
+  ];
+
+  function full() {
+    if (!boardArray.includes("")) {
+      return true;
+    }
+  }
+
+  return { populateArray, possibleWins, full };
+});
 
 // Game control module
 const gameController = (() => {
   let currentPlayer;
 
-  function newGame() {
+  (function newGame() {
+    displayController.reset();
     getNames();
     currentPlayer = player1;
-    displayController.setPlayerMsg(currentPlayer.name);
-    displayController.displayPlayerMsg();
+    displayController.setUpNext(currentPlayer.name);
+    displayController.displayUpNext();
     playTurn();
-  }
+  })();
 
   function getNames() {
     let name;
@@ -69,7 +114,12 @@ const gameController = (() => {
   }
 
   function playTurn() {
-    if (!gameOver()) {
+    if (winner()) {
+      displayController.gameOverMessage(currentPlayer);
+      displayController.deactivateSquares();
+    } else if (board.full()) {
+      displayController.gameOverMessage();
+    } else {
       playerMove();
     }
   }
@@ -80,34 +130,31 @@ const gameController = (() => {
         if (square.textContent === '') {
           displayController.addToken(square, currentPlayer.token);
           switchPlayer();
-          displayController.setPlayerMsg(currentPlayer.name);
+          displayController.setUpNext(currentPlayer.name);
           playTurn();
         }
       })
     })
   }
 
-  function gameOver() {
-    const possibleWins = [
-      [0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7],
-      [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]
-    ]
+  function winner() {
+    let winner;
     let currentBoard = board.populateArray();
-    possibleWins.forEach(array => {
-      if (currentBoard[array[0]] === currentBoard[array[1]] === currentBoard[array[2]]) {
-        return true;
+    board.possibleWins.forEach(array => {
+      let base = currentBoard[array[0]];
+      if (base === currentBoard[array[1]]
+        && base === currentBoard[array[2]] 
+        && base !== "") {
+          switchPlayer();
+          winner = true;
+          return;
       }
     })
-    return false;
+    return winner;
   }
 
   function switchPlayer() {
     currentPlayer = (currentPlayer === player1) ? player2 : player1;
-  }
-
-  return {
-    newGame,
-    currentPlayer
   }
 });
 
